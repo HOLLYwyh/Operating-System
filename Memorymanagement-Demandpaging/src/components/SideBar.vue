28e87128aab5d651da5c168950d86fc4ce9c225d
HOLLYwyh <1689394668@qq.com>
2021-06-05 12:42:02 +0800

    Revert "Revert "initialize SideBar""
    This reverts commit dbef824c205830980d654a1d37ace4196aa9417b.

---
+++ Memorymanagement-Demandpaging/src/components/SideBar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+      <!--这是一个侧边栏 -->
+    <div>
+      <ul  class="sidebar">
+        <li>
+          <a href="" class="block p-4 border-r-4 border-purple-400 text-base text-gray-gray-400 font-bold hover:border-purple-500 hover:bg-purple-100">商品管理</a>
+        </li>
+        <li>
+          <a href="" class="block p-4 border-r-4 border-transparent text-base text-gray-gray-400 font-bold hover:border-purple-500 hover:bg-purple-100">系统管理</a>
+        </li>
+        <li>
+          <a href="" class="block p-4 border-r-4 border-transparent text-base text-gray-gray-400 font-bold hover:border-purple-500 hover:bg-purple-100">分类管理</a>
+        </li>
+        <li>
+          <a href="" class="block p-4 border-r-4 border-transparent text-base text-gray-gray-400 font-bold hover:border-purple-500 hover:bg-purple-100">联系我们</a>
+        </li>
+        <li>
+          <a href="" class="block p-4 border-r-4 border-transparent text-base text-gray-gray-400 font-bold hover:border-purple-500 hover:bg-purple-100">联系我们</a>
+        </li>
+        <li>
+          <a href="" class="block p-4 border-r-4 border-transparent text-base text-gray-gray-400 font-bold hover:border-purple-500 hover:bg-purple-100">联系我们</a>
+        </li>
+        <li>
+          <a href="" class="block p-4 border-r-4 border-transparent text-base text-gray-gray-400 font-bold hover:border-purple-500 hover:bg-purple-100">联系我们</a>
+        </li>
+        <li>
+          <a href="" class="block p-4 border-r-4 border-transparent text-base text-gray-gray-400 font-bold hover:border-purple-500 hover:bg-purple-100">联系我们</a>
+        </li>
+        <p>你好</p>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SideBar'
+}
+</script>
+
+<style scoped>
+.sidebar{
+    position: fixed;
+    width: 400px;
+    height :100%;
+}
+</style>
--- memorymanagement-demandpaging/src/App.vue
+++ memorymanagement-demandpaging/src/App.vue
@@ -1,27 +1,20 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SideBar/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SideBar from './components/SideBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SideBar
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
