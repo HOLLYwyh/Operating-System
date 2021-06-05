dbef824c205830980d654a1d37ace4196aa9417b
HOLLYwyh <1689394668@qq.com>
2021-06-05 12:40:47 +0800

    Revert "initialize SideBar"
    This reverts commit e65996128972f4c5cfd0cde3b87f67600a3bf5a5.

--- memorymanagement-demandpaging/src/App.vue
+++ memorymanagement-demandpaging/src/App.vue
@@ -1,20 +1,27 @@
 <template>
   <div id="app">
-    <SideBar/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import SideBar from './components/SideBar.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    SideBar
+    HelloWorld
   }
 }
 </script>
 
 <style>
-
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
