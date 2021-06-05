6abe4d5d9cc17365d30dbd0eb25b9f7ef85cb50f
HOLLY wyh <64917442+HOLLYwyh@users.noreply.github.com>
2021-06-05 12:48:18 +0800

    Add files via upload

---
+++ memorymanagement-demandpaging/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <SideBar/>
+  </div>
+</template>
+
+<script>
+import SideBar from './components/SideBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SideBar
+  }
+}
+</script>
+
+<style>
+
+</style>
